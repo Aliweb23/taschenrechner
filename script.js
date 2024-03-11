a430040f88ff6e12991b87b14d57323c23231619
Aliweb23 <haschimi7219@gmail.com>
2024-03-11 22:53:08 +0100

    smallfix

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Wähle die Anzeige des Taschenrechners aus
     const display = document.querySelector('.display');
-
-    // Wähle alle Buttons aus
     const buttons = document.querySelectorAll('.pad button');
 
-    // Setze den aktuellen Wert der Anzeige
     let currentValue = '0';
 
-    // Maximale Anzahl von Ziffern, die in der Anzeige angezeigt werden sollen
+
     const maxDisplayLength = 10;
 
-    // Funktion zum Begrenzen der Anzahl von Ziffern
+
     function limitDisplayLength(value) {
         return value.toString().slice(0, maxDisplayLength);
     }
 
-    // Hinzufügen eines Event Listeners für jeden Button
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             const buttonText = button.textContent;
@@ -33,11 +28,10 @@ document.addEventListener('DOMContentLoaded', function () {
                     currentValue = 'Fehler';
                 }
             } else {
-                // Füge den gedrückten Button-Text zur aktuellen Anzeige hinzu
+              
                 currentValue = limitDisplayLength(currentValue === '0' ? buttonText : currentValue + buttonText);
             }
 
-            // Aktualisiere die Anzeige
             display.textContent = currentValue;
         });
     });
